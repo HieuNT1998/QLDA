3646cf00011c10a7563fcbbf4fdb612909742ba0
HieuNT1998 <trihieua2@gmail.com>
2019-11-28 15:11:43 +0700

    flask card

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_070531) do
+ActiveRecord::Schema.define(version: 2019_11_24_154223) do
+
+  create_table "hiraganas", force: :cascade do |t|
+    t.string "cachviet"
+    t.string "cachdoc"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
